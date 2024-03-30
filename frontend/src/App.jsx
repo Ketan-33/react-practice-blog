871a0b5079a8ba4c5d0ff328a2e0c2cd83fd7d88
ketan <ketanpardhi04@gmail.com>
2024-03-30 15:37:02 +0530

    shared component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,21 @@ import Home from "./home/pages/Home";
 import UserList from "./admin/users/pages/UserList";
 
 import Login from "./admin/users/pages/Login";
+import Sidebar from "./shared/components/SidebarNav"
 
 
 function App() {
   return (
-      <Router>
+    <div> <Sidebar/>
+          <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/users" element={<UserList />} />
       </Routes>
     </Router>
+    </div>
+
   )
 }
 
