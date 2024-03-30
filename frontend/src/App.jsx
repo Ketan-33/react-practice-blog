2632a3d7c5563e74aaec5ea192ccebb5b0c87672
ketan <ketanpardhi04@gmail.com>
2024-03-30 15:32:04 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
-const App =()=>{
-  return <div className="myDiv">Hello Ketan</div>
-};
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./home/pages/Home";
+import UserList from "./admin/users/pages/UserList";
 
-export default App;
+import Login from "./admin/users/pages/Login";
+
+
+function App() {
+  return (
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/users" element={<UserList />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
